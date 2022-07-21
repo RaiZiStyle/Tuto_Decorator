ffff690e8c63f3f9cb8a4d6bd6c9cafb172ae6c6
Arthur G.P <arthur.guyotpremel@gmail.com>
2022-07-21 16:51:15 +0200

    Step 2

--- Tuto1.py
+++ Tuto1.py
@@ -2,27 +2,66 @@
 
 import functools
 import logging
+from typing import Union
 
-logging.basicConfig(level = logging.DEBUG)
-logger = logging.getLogger()
+class MyLogger:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        
+        # create a file handler
+        handler_file = logging.FileHandler("test_log.log")
+        handler_file.setLevel(logging.DEBUG)
 
-def log(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        args_repr = [repr(a) for a in args]
-        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
-        signature = ", ".join(args_repr + kwargs_repr)
-        logger.debug(f"function {func.__name__} called with args {signature}")
-        try:
-            result = func(*args, **kwargs)
-            return result
-        except Exception as e:
-            logger.exception(f"Exception raised in {func.__name__}. exception: {str(e)}")
-            raise e
-    return wrapper
+        # create stdout handler
+        handler_stdout = logging.StreamHandler()
+        formatter = logging.Formatter('[%(asctime)s - %(name)s - %(levelname)s] %(message)s')
+        handler_stdout.setFormatter(formatter)
+        handler_stdout.setLevel(logging.DEBUG)
 
-@log
+        # create a logging format
+        formatter = logging.Formatter('[%(asctime)s - %(name)s - %(levelname)s] %(message)s')
+        handler_file.setFormatter(formatter)
+
+        # add the file handler to the logger
+        self.logger.addHandler(handler_file)
+        self.logger.addHandler(handler_stdout)
+        
+
+    def get_logger(self, name=None):
+        return logging.getLogger(name)
+
+def get_default_logger():
+    return MyLogger().get_logger()
+
+def log(_func=None, *, my_logger: Union[MyLogger, logging.Logger] = None):
+    def decorator_log(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            if my_logger is None:
+                logger = get_default_logger()
+            else:
+                if isinstance(my_logger, MyLogger):
+                    logger = my_logger.logger
+            args_repr = [repr(a) for a in args]
+            kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+            signature = ", ".join(args_repr + kwargs_repr)
+            logger.debug(f"function {func.__name__} called with args {signature}")
+            try:
+                result = func(*args, **kwargs)
+                return result
+            except Exception as e:
+                logger.exception(f"Exception raised in {func.__name__}. exception: {str(e)}")
+                raise e
+        return wrapper
+
+    if _func is None:
+        return decorator_log
+    else:
+        return decorator_log(_func)
+
+@log(my_logger=MyLogger())
 def sum(a, b=10):
     return a + b
 
-sum(10)
+sum(10 , b=5)
