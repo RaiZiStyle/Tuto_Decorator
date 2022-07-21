88b4e764160cd5ffa081d50bd8f0325120782e7e
Arthur G.P <arthur.guyotpremel@gmail.com>
2022-07-21 16:31:56 +0200

    Step 1

---
+++ Tuto1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import functools
+import logging
+
+logging.basicConfig(level = logging.DEBUG)
+logger = logging.getLogger()
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        logger.debug(f"function {func.__name__} called with args {signature}")
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except Exception as e:
+            logger.exception(f"Exception raised in {func.__name__}. exception: {str(e)}")
+            raise e
+    return wrapper
+
+@log
+def sum(a, b=10):
+    return a + b
+
+sum(10)
